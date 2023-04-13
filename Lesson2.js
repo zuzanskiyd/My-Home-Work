68dec91e1ea9df0ea0c0525c338cdb276bbf64a8
zuzanskiyd <130315202+zuzanskiyd@users.noreply.github.com>
2023-04-13 15:12:31 +0300

    Add files via upload

---
+++ Lesson2.js
@@ -0,0 +1,115 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// console.log(1 > 0)
+// console.log(0 > 0);
+// console.log(-3 > 0);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// let word = "test";
+
+// console.log(word === 'test');
+// console.log(word === 'qwerty');
+// console.log(word === true);
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// let num = 1;
+
+// if (num >= 10) {
+//     num = num - 5;
+// } else {
+//     num = num + 5;
+// }
+
+// console.log(num);
+
+// let num = 10;
+
+// if (num >= 10) {
+//   num = num - 5;
+// } else {
+//   num = num + 5;
+// }
+
+// console.log(num);
+
+// let num = 13;
+
+// if (num >= 10) {
+//   num = num - 5;
+// } else {
+//   num = num + 5;
+// }
+
+// console.log(num);
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const num = prompt('Введіть ціле число від 1 до 12:');
+// let mounth;
+
+// switch (num) {
+//   case 1:
+//     mounth = 'January';
+//     alert(`${mounth}`);
+//     break;
+//   case 2:
+//     mounth = 'February';
+//     alert(`${mounth}`);
+//     break;
+//   case 3:
+//     mounth = 'March';
+//     alert(`${mounth}`);
+//     break;
+//   case 4:
+//     mounth = 'April';
+//     alert(`${mounth}`);
+//     break;
+//   case 5:
+//     mounth = 'May';
+//     alert(`${mounth}`);
+//     break;
+//   case 6:
+//     mounth = 'June';
+//     alert(`${mounth}`);
+//     break;
+//   case 7:
+//     mounth = 'July';
+//     alert(`${mounth}`);
+//     break;
+//   case 8:
+//     mounth = 'August';
+//     alert(`${mounth}`);
+//     break;
+//   case 9:
+//     mounth = 'September';
+//     alert(`${mounth}`);
+//     break;
+//   case 10:
+//     mounth = 'October';
+//     alert(`${mounth}`);
+//     break;
+//   case 11:
+//     mounth = 'November';
+//     alert(`${mounth}`);
+//     break;
+//   case 12:
+//     mounth = 'December';
+//     alert(`${mounth}`);
+//     break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let num = prompt('Ввудіть ціле тризначне число:');
+
+// let sum = parseInt(num[0]) + parseInt(num[1]) + parseInt(num[2]);
+
+// alert(`Сума цифр введеного вами числа ${num} складає ${sum}`);
