b9312c1e8e6f905394a7b1f97ce1500ef54bb415
zuzanskiyd <130315202+zuzanskiyd@users.noreply.github.com>
2023-04-29 19:04:16 +0300

    Add files via upload

---
+++ Lesson6.js
@@ -0,0 +1,32 @@
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+// class Abonent {
+//   constructor(name, number) {
+//     this.name = name;
+//     this.number = number;
+//   }
+
+//   set(name, number) {
+//     this.name = name;
+//     this.number = number;
+//   }
+
+//   get() {
+//     console.log(`Name: ${this.name}, Number: ${this.number}`);
+//   }
+// }
+
+// const abonent1 = new Abonent('Dima', '+380630000001');
+// const abonent2 = new Abonent('Misha', '+380630000002');
+// const abonent3 = new Abonent('Bogdan', '+380630000003');
+
+// abonent1.get();
+// abonent2.get();
+// abonent3.get();
+
+// abonent1.set('Diana', '+380630000004');
+// abonent1.get();
